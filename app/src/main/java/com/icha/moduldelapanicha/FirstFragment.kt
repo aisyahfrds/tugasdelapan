87470351d5b989475287e8aecbf0ed8b9a50e5ae
aisyahfrds <aisyahfirdausi73@gmail.com>
2020-04-05 22:03:08 +0700

    commit

---
+++ app/src/main/java/com/icha/moduldelapanicha/FirstFragment.kt
@@ -0,0 +1,31 @@
+package com.icha.moduldelapanicha
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+
+import com.google.android.material.textfield.TextInputEditText
+
+class FirstFragment : Fragment() {
+    private var communicationViewModel: CommunicationViewModel? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        communicationViewModel =
+            ViewModelProviders.of(requireActivity()).get(CommunicationViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_first,
+            container, false
+        )
+    }
+}
